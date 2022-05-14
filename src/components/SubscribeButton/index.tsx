8eb73256226566d98e88c609b6debbbc24851154
muneves3 <murilogrilosn@gmail.com>
2022-05-14 15:38:15 -0300

    feat: adding the github login button and home

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  const isUserLoggedIn = false;
+
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
