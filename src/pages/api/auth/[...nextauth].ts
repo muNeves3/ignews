c82e00048eb8b6cb384de308ec6d2b44ba039736
muneves3 <murilogrilosn@gmail.com>
2022-05-19 17:06:23 -0300

    feat: adding the first fauna query, but github signin is not working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,37 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb'
 
 export default NextAuth({
     providers: [
         GithubProvider({
-            clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            authorization: 'https://github.com/login/oauth/authorize?scope=read:user+user:email',
+            token: "https://github.com/login/oauth/access_token",
+            checks: 'both',
         }),
     ],
-    secret: process.env.SECRET
+    jwt:{
+        secret: process.env.SIGNIN_KEY
+    },
+    callbacks: {
+        async signIn({user, account, profile}) {
+            const { email } = user;
+           try {
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { email } }
+                ),
+            )
+            return true
+           } catch {
+               return false
+           }
+        }
+    },
+    //secret: process.env.SECRET,
 
 })
